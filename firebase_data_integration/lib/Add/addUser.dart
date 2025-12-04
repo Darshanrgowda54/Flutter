0264b1c9d1c656b290f1bb1250e0694cb507e59c
Darshanrgowda54 <darshan.ramesh565@gmail.com>
2025-12-04 17:04:38 +0530

    update some ui changes in addUser.dart file

--- firebase_data_integration/lib/Add/addUser.dart
+++ firebase_data_integration/lib/Add/addUser.dart
@@ -39,87 +39,96 @@ class _AddUserState extends State<AddUser> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          SizedBox(height: 80),
-          TextField(
-            controller: nameController,
-            decoration: InputDecoration(
-              hintText: "Name",
-              contentPadding: EdgeInsets.all(12),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(4)),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Add Users to List",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // SizedBox(height: 80),
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                hintText: "Name",
+                contentPadding: EdgeInsets.all(12),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(4)),
+                ),
               ),
             ),
-          ),
 
-          SizedBox(height: 20),
-          TextField(
-            controller: numberController,
-            decoration: InputDecoration(
-              hintText: "Phone Number",
-              contentPadding: EdgeInsets.all(12),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(4)),
+            SizedBox(height: 20),
+            TextField(
+              controller: numberController,
+              decoration: InputDecoration(
+                hintText: "Phone Number",
+                contentPadding: EdgeInsets.all(12),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(4)),
+                ),
               ),
             ),
-          ),
 
-          SizedBox(height: 20),
-          TextField(
-            controller: emailController,
-            decoration: InputDecoration(
-              hintText: "Email",
-              contentPadding: EdgeInsets.all(12),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(4)),
+            SizedBox(height: 20),
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(
+                hintText: "Email",
+                contentPadding: EdgeInsets.all(12),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(4)),
+                ),
               ),
             ),
-          ),
 
-          SizedBox(height: 20),
-          TextField(
-            controller: rollnumberController,
-            decoration: InputDecoration(
-              hintText: "Roll Number",
-              contentPadding: EdgeInsets.all(12),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(4)),
+            SizedBox(height: 20),
+            TextField(
+              controller: rollnumberController,
+              decoration: InputDecoration(
+                hintText: "Roll Number",
+                contentPadding: EdgeInsets.all(12),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(4)),
+                ),
               ),
             ),
-          ),
 
-          SizedBox(height: 20),
-          TextField(
-            controller: addressController,
-            decoration: InputDecoration(
-              hintText: "Address",
-              contentPadding: EdgeInsets.all(12),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(4)),
+            SizedBox(height: 20),
+            TextField(
+              controller: addressController,
+              decoration: InputDecoration(
+                hintText: "Address",
+                contentPadding: EdgeInsets.all(12),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(4)),
+                ),
               ),
             ),
-          ),
 
-          SizedBox(height: 20),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blue[800],
-              foregroundColor: Colors.white,
-              minimumSize: Size(400, 50),
+            SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue[800],
+                foregroundColor: Colors.white,
+                minimumSize: Size(400, 50),
+              ),
+              onPressed: () {
+                debugPrint("Submit Button Clicked");
+                submitClicked();
+              },
+              child: Text(
+                "Submit",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
             ),
-            onPressed: () {
-              debugPrint("Submit Button Clicked");
-              submitClicked();
-            },
-            child: Text(
-              "Submit",
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
